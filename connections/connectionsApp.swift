56c5bb0f5e017ab4441618b768d13f3d88d088ac
Sam <sikora.samj@gmail.com>
2023-02-11 19:42:55 -0800

    Initial Commit

---
+++ connections/connectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  connectionsApp.swift
+//  connections
+//
+//  Created by Sam Sikora on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct connectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
